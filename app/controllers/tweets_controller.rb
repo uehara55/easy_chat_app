51cc5b31e121266795c94c0afbb561987bad60ce
uehara55 <yusukeuehara55@gmail.com>
2018-12-15 15:49:36 +0900

    Add post function

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,7 +1,19 @@
 class TweetsController < ApplicationController
 
   def index
-    @tweets = Tweet.all
+    @tweets = Tweet.all.order(created_at: "desc").page(params[:page]).per(5)
+  end
+
+  def new
+  end
+
+  def create
+    Tweet.create(tweet_params)
+  end
+
+  private
+  def tweet_params
+    params.permit(:name, :image, :text)
   end
 
 end
